e996f68c4536f45f0f4fe64b94966a6f7a90392e
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 10:39:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,20 +3,6 @@ def nyc_pigeon_organizer(data)
  data.each do |key, value|
    value.each do |new_value, names|
   
-  names.each do |name|
-   if !pigeon_hash[name] 
-     pigeon_hash[name] = {}
-  end
- if !pigeon_hash[name][key]
-   pigeon_hash[name][key] = []
-end
-pigeon_hash[name][key] << new_value.to_s
 
-  end
-  
-end
-pigeon_hash
-end
-  
   
 end
